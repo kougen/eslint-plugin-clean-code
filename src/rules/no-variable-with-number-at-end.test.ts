2ea1da5a33ea6faf0271e2137365036f0c7dc11b
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-03-19 12:15:13 +0100

    feat: tests improvements

--- src/rules/no-variable-with-number-at-end.test.ts
+++ src/rules/no-variable-with-number-at-end.test.ts
@@ -1,4 +1,4 @@
-import { RuleTester } from '@typescript-eslint/rule-tester';
+import {RuleTester} from '@typescript-eslint/rule-tester';
 import noVariableWithNumberAtEnd from "./no-variable-with-number-at-end";
 
 
@@ -13,12 +13,32 @@ ruleTester.run(
   "no-number",
   noVariableWithNumberAtEnd,
   {
-    valid: [{
-      code: "const foo = 'bar';",
-    }],
+    valid: [
+      {code: "const foo = 'bar';", filename: "file.ts"},
+      {code: "const foo123 = 'bar';", filename: "ignore.txt"}
+    ],
     invalid: [{
       code: "const foo2 = 'bar';",
-      errors: [{ messageId: "noNumberEnding" }],
+      errors: [{messageId: "noNumberEnding"}],
     }],
+  }
+);
+
+ruleTester.run(
+  "no-number (custom extensions)",
+  noVariableWithNumberAtEnd,
+  {
+    valid: [
+      {code: "const foo = 'bar';", filename: "file.ts", options: [{extensions: [".ts"]}]},
+      {code: "const foo123 = 'bar';", filename: "file.js", options: [{extensions: [".ts"]}]},
+    ],
+    invalid: [
+      {
+        code: "const foo2 = 'bar';",
+        filename: "file.ts",
+        options: [{extensions: [".ts"]}],
+        errors: [{messageId: "noNumberEnding"}],
+      }
+    ],
   }
 );
